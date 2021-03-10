328559c1b287c6c3012e71c8886f6f8e44c5a271
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-03-10 16:52:15 +0500

    Appbar >> Navbar component created. CartDrawer component created ----------------------------------------------------------------

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.scss";
-import { Route } from "react-router-dom";
-// import Login from "./pages/auth";
-// import Register from "./pages/auth";
+import { Route, useLocation } from "react-router-dom";
 import { Login, Register } from "./pages/auth";
+import Home from "./pages/home";
+import DrawerCard from "./components/navbars/CartDrawer";
+import Appbar from "./components/navbars/Appbar";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
+      <Route path="/" exact component={Home} />
+
+      {location.pathname === "/login" || location.pathname === "/register" || (
+        <>
+          <Appbar />
+          <DrawerCard />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/navbars/Appbar.jsx
@@ -0,0 +1,40 @@
+import { AppBar, makeStyles, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: "auto",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function Appbar() {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" noWrap>
+          Permanent drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
