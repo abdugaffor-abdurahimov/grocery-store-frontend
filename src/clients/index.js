96e1f4c556f7028c075f72d6b659453fc9ff0963
Abdugaffor <abdurahimov.97@list.ru>
2021-03-12 09:49:59 +0500

    User actions created

--- src/clients/index.js
+++ src/clients/index.js
@@ -1,7 +1,15 @@
 import axios from "axios";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const fetchDefault = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: apiUrl,
+});
+
+export const fetchWithTokens = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
 });
 
 export default fetchDefault;
